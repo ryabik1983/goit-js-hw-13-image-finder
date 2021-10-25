eab4fddb9638ba2130b7619c8b780140dd776073
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2021-10-25 16:23:14 +0300

    13
    13

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,3 +1,36 @@
-
-
 const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ'; 
+const API_KEY = '23935285-7259499af9fe05c3ca983c7fa';
+const BASE_URL = 'https://pixabay.com/api/';
+const MAIN_SEARCH_SETTINGS = '?image_type=photo&orientation=horizontal&';
+
+export default class ApiImagesService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async onFetchImages() {
+    const url = `${BASE_URL}${MAIN_SEARCH_SETTINGS}q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    return await fetch(url).then(response => {
+      this.incrementPage();
+      return response.json();
+    });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
